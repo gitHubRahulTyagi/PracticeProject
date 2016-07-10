3bb309e1b365bd1da1a2fdae8c6cef5fad263ca7
unknown <vkriat@gmail.com>
2016-07-10 02:09:39 -0700

    first commit

---
+++ src/test_1.java
@@ -0,0 +1,65 @@
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class test_1 {
+
+	
+	@Test
+	public void testLogin(){
+		
+		System.out.println("Testing Login test case");
+		
+	}
+	
+	
+	@Test
+	public void testUserregisteration(){
+		
+		System.out.println("Testing User registeration");
+	}
+	
+	@BeforeMethod
+	public void Openbrowser(){
+		
+		System.out.println("Open broswser");		
+	}
+	
+	@BeforeTest
+	public void openDb(){
+		
+		System.out.println("Open db connection");
+	}
+	
+	@AfterMethod
+	public void Closebrowser(){
+		
+		System.out.println("Close browsers");
+		
+	}
+	
+	@AfterTest
+	public void closeDB(){
+		
+		System.out.println("Close db connection");
+	}
+	
+	
+	@BeforeSuite
+	public void runSeleniumserver(){
+		
+		System.out.println("Testing running of selenium server");
+	}
+	
+	@AfterSuite
+	public void stopSeleniumserver(){
+		
+		System.out.println("Close selenium server");
+	}
+	
+}
